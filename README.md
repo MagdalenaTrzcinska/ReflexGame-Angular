7c64a33106dce6a0427cf141a007a5eb2e0ba64d
Madga Trzcinska <magdatrzcinska23@gmail.com>
2020-04-15 11:38:28 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Reflex
+ng# Reflex
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivePktTimeComponent } from './live-pkt-time/live-pkt-time.component';
+import { BoardGameComponent } from './board-game/board-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivePktTimeComponent,
+    BoardGameComponent
   ],
   imports: [
     BrowserModule,
